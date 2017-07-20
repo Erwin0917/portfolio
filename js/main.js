0662b528abfa2e69809fd87de3a5f04042f8def1
Erwin Heba <erwin0917@gmail.com>
2017-07-20 15:27:39 +0200

    skills v1.1

--- js/main.js
+++ js/main.js
@@ -1,30 +1,95 @@
 (function($){
 
+function debounce(callback, wait) {
+	var timeout;
+	return function() {
+		var ctx = this, args = arguments;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+		    callback.apply(ctx, args);
+		}, wait || 100);
+	};
+};
+function resizeCheck(obj, callback){
+    $(obj).resize(callback);
+}
+
+
+
 $(document).ready(function() {
     //Loader ---->
     $(".loader").stop().fadeOut("slow", function(){
         $(".loader").addClass("hidden");
     });
-    //Loader End ----
     
+    
+
+
+    // add mobile class to body
+    function addMobileClass(){
+        if($(window).width() < "1200"){     
+         $("body").addClass("mobile");
+        }else{
+         $("body").removeClass("mobile");   
+        }
+    };
+
+    debounce(resizeCheck(window,addMobileClass), 200);
+
+    addMobileClass();
+
 
     //mobile menu ---->
     var btn = $(".nav-mobile-btn");
-    
-    btn.click(function(){
+
+    function toggleMobileMenu(){
         if(btn.text() == "Menu"){
-            btn.text("Zamknij");
-            $(".nav-container").stop().fadeIn("slow", function(){})
-        }else {
-            btn.text("Menu");
-            $(".nav-container").stop().fadeOut("slow", function(){})
-        }
-    });
-    $(".nav-container a").click(function(){
-            btn.text("Menu");
-            $(".nav-container").stop().fadeOut("slow", function(){})
-    });
+                btn.text("Zamknij");
+                $(".nav-container").stop().fadeIn("slow", function(){})
+            }else {
+                btn.text("Menu");
+                $(".nav-container").stop().fadeOut("slow", function(){})
+            }
+           
+    }
+   
+    btn.click(toggleMobileMenu);
+
+    debounce(resizeCheck(window, function(){
+        return function(){
+            console.log("work");
+        };
         
+    }), 1000);
+
+
+    debounce(resizeCheck(window, function(){// zamykanie menu po kliknieciu w link tylko dla mobile
+        return function(){
+            if($(window).width() < "1200"){
+            console.log("działa");
+            $(".nav-container a").click(function(){
+                alert("dziala");
+            })
+        $(".nav-container a").click(function(){
+                btn.text("Menu");
+                $(".nav-container").stop().fadeOut("slow", function(){})
+            });
+        }
+        }
+        
+
+    }), 200 );
+    
+   
+
+
+   debounce(resizeCheck(window, function(){ // jesli menu w mobile ukryte i jest powiekszenie okna przeglądarki to usuniecie display: none
+        if( !$('body').hasClass("mobile") ){
+        $(".nav-container").show();
+        
+    }
+    }), 200 );         
+       
     
     
 
