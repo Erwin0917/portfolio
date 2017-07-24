1ee151339b53d44a170805a8d29cde6092cd9b86
Erwin Heba <erwin0917@gmail.com>
2017-07-24 13:39:51 +0200

    contact beginn

--- js/main.js
+++ js/main.js
@@ -81,10 +81,10 @@ $(document).ready(function() {
 
 
 
-// smaller menu 
+// smaller menu  ---- set smaller menu after scroll on section 2. 
 function smallerMenu(){
     var menu = $(".nav-container"),
-        headerHeight = $("#header").innerHeight(),
+        headerHeight = $("#home").innerHeight(),
         scrollPos = $(window).scrollTop();
 
     $(window).scroll(function (e) {
@@ -103,5 +103,56 @@ function smallerMenu(){
 }
 
 smallerMenu();
+
+
+
+// smooth scroll
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+  //disable link
+  $("a").click(
+      function(e){
+        let target = e.target;
+
+        if( $(target).hasClass("disable") )
+            event.preventDefault();
+      }
+  )
 
 })(jQuery);
